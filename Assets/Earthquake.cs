48b889df14855be1f233ad336f4d7a2dc6457e21
sebasexy <spider_cbaz_10@hotmail.com>
2016-04-27 14:43:43 -0500

    Funcional

---
+++ Assets/Earthquake.cs
@@ -0,0 +1,67 @@
+﻿		using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Earthquake : MonoBehaviour {
+
+	public InputField peso, k;
+	public GameObject piso;
+	public GameObject anchor;
+	private List<Piso> pisos;
+
+	void Awake() {
+		
+		pisos = new List<Piso> ();
+
+
+	}
+
+	public void Test(Text a, Text b) {
+		
+	}
+
+	public void AddFloor(){
+
+
+		/*
+		this.weight = int.Parse(weight);
+		this.k = int.Parse(k);
+		this.cantNiveles++;*/
+		Debug.Log (peso.text);
+		Debug.Log (k.text);
+
+		Vector3 pos = anchor.transform.position;
+		pos.y = pos.y + pisos.Count * 50;
+
+		GameObject go = Instantiate (piso, pos, anchor.transform.rotation) as GameObject;
+		go.transform.parent = anchor.transform.parent;
+		pisos.Add(new Piso(int.Parse(peso.text), int.Parse(k.text), go));
+
+	}
+
+	public double[] Finalizar(){
+		/*
+		pisos = new double[niveles];
+		return pisos;
+		*/
+
+		double[] resultado = new double[pisos.Count];
+		for (int i = 0; i < pisos.Count; i++) {
+		
+			resultado[i] = pisos[i].Peso;
+		}
+		return resultado;
+	}
+
+	public void Destruir(){
+
+		for (int i = 0; i < pisos.Count; i++) {
+			
+			Destroy (pisos [i].piso);
+
+		}
+
+		pisos = new List<Piso> ();
+	}
+}
---
+++ Assets/Piso.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piso {
+
+	private int peso,
+	k;
+
+	public int Peso {
+
+		get { 
+			return peso;
+		}
+	}
+
+	public GameObject piso;
+
+	public Piso(int peso, int k, GameObject piso){
+		this.peso = peso;
+		this.k = k;
+		this.piso = piso;
+	}	 
+}
